bb48cd7f75d12a84271851339cd81e756500f6bf
Cornellius Ngondo <devngondo@gmail.com>
2015-04-21 17:18:16 +0000

    Modified files in preperation for the login activity. Created new java class

--- app/src/main/java/mpishi/example/ngondo/Login_Activity.java
+++ app/src/main/java/mpishi/example/ngondo/Login_Activity.java
@@ -5,19 +5,21 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.facebook.FacebookSdk;
+
 
 public class Login_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //getActionBar().hide();
+//        Let's first hide the action bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
     }
 
